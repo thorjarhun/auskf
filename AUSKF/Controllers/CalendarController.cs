b3501e2303399862492d886688153822a8a4cb61
Steve Hoff <smhoff256@gmail.com>
2016-01-11 16:21:58 -0600

    Adding Calendar Controller/View

---
+++ AUSKF/Controllers/CalendarController.cs
@@ -0,0 +1,13 @@
+﻿namespace AUSKF.Controllers
+{
+    using System.Web.Mvc;
+
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
