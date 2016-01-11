f039f76182ba55db7936d8730bd1063ca4eb9f80
Steve Hoff <smhoff256@gmail.com>
2016-01-11 16:14:40 -0600

    Addin all endpoints for the Iaido area

---
+++ AUSKF/Areas/Iaido/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿namespace AUSKF.Areas.Iaido.Controllers
+{
+    using System;
+    using System.IO;
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        // GET: Iaido/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Study()
+        {
+            return View();
+        }
+
+        public ActionResult Points()
+        {
+            return View();
+        }
+
+        public ActionResult History()
+        {
+            return View();
+        }
+
+        public ActionResult ChampionshipResults()
+        {
+            return View();
+        }
+
+        public ActionResult Archives()
+        {
+            return View();
+        }
+
+        public FileResult ShinsaQuestions()
+        {
+            return File(AppDomain.CurrentDomain.BaseDirectory + 
+                "\\Content\\pdfs\\2010_iaido_questions.pdf", "application/pdf");
+        }
+    }
+}
---
+++ AUSKF/Areas/Iaido/IaidoAreaRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace AUSKF.Areas.Iaido
+{
+    public class IaidoAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Iaido";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+
+            context.MapRoute(
+                name: "Iaido_default",
+                url: "Iaido/{action}/{id}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "AUSKF.Areas.Iaido.Controllers" }
+            );
+        }
+    }
+}
