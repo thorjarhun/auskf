ea0a8c6aec762b98ca0fbdbe756f138e3b1c563d
Steve Hoff <smhoff256@gmail.com>
2016-01-11 20:25:57 -0600

    Adding lots of stuff

---
+++ AUSKF/Areas/Forms/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿namespace AUSKF.Areas.Forms.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Get(string form)
+        {
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "Areas\\Forms\\Content\\" + form;
+            if (!System.IO.File.Exists(filePath))
+            {
+                return new HttpNotFoundResult("optional description");
+
+            }
+            return File(filePath, "application/pdf");
+        }
+    }
+}
